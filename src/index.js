9a95f4c7c957627aae704cd34f8e3ff2478523bc
7r0n1x <techsoftec.tronix@gmail.com>
2022-07-15 15:14:59 -0500

    Updated structure

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ const socketio = require('socket.io');
 const app = express();
 const server = http.createServer(app);
 const io = socketio.listen(server);
+const path = require('path');
 
 app.set('port', process.env.PORT || 4000);
 
 require('./socket')(io, app);
 
-app.use(express.static('./public'));
+app.use(express.static(path.join(__dirname, '../public')));
 
 server.listen(app.get('port'), () => {
     console.log('Server on port: ' + app.get('port'));
